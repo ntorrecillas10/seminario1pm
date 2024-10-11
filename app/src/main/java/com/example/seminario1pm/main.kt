4172fd2bc9740b35f65074a7d5fdb629f40ea541
ntorrecillas10 <nalascobic7@gmail.com>
2024-10-11 10:01:58 +0200

    ej9y10

--- app/src/main/java/com/example/seminario1pm/main.kt
+++ app/src/main/java/com/example/seminario1pm/main.kt
@@ -15,7 +15,9 @@ fun main(){
     ej4(cadena)
 println()
     println(ej5("Viva el vino",'i'))
-
+    esprimo(0)
+    ej9(32,40)
+    ej10()
 }
 
 fun ej1(lista: IntArray){
@@ -65,3 +67,47 @@ fun ej5(ejercicio5: String, letra: Char):Int{
     return contador
 }
 
+fun esprimo(num: Int) {
+    val num = 37
+    var i = 2
+    var flag = false
+    while (i <= num / 2) {
+        // condition for nonprime number
+        if (num % i == 0) {
+            flag = true
+            break
+        }
+        ++i
+    }
+
+    if (!flag)
+        println("$num is a prime number.")
+    else
+        println("$num is not a prime number.")
+}
+
+fun ej9(num1: Int, num2: Int) {
+    var mcd = 0
+    var i = if (num1 < num2) num1 else num2
+
+    while (i > 0) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            mcd = i
+            break
+        }
+        i--
+    }
+
+    println("El máximo común divisor de $num1 y $num2 es $mcd")
+}
+
+fun ej10(){
+    var enesimo = 0
+    var enesimo2 = 1
+    var i = 2
+    while (i<=9){
+        enesimo2 = enesimo + enesimo2
+        i++
+    }
+    println(enesimo)
+}
